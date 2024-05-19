84293d3ff019101e78d1b57d3143a731fd0ff560
yash parmar <166715506+yashsinghparmar@users.noreply.github.com>
2024-05-19 11:56:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-box, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'top' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
